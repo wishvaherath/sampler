e34665eb0d0eea3b10c39a4717b973657f0d630c
Wishva <wishvah@gmail.com>
2019-04-17 12:47:27 +1000

    trying to correct the issue of prematurely sampling

--- downsampler_simple.py
+++ downsampler_simple.py
@@ -7,19 +7,19 @@ import random
 
 #constants
 RESOLUTION = 100
-LINES_TO_BATCH = 100
+ENTRIES_TO_BATCH = 100
 BUFFER = True
 
 gz_file_name_1 = sys.argv[1].strip()
 gz_file_name_2 = sys.argv[2].strip()
 total_size = int(sys.argv[3].strip())
 sample_size = int(sys.argv[4].strip())
-LINES_TO_BATCH = int(sys.argv[5].strip())
+ENTRIES_TO_BATCH = int(sys.argv[5].strip())
 gz_1 = gzip.open(gz_file_name_1,'rb')
 gz_2 = gzip.open(gz_file_name_2,'rb')
 if BUFFER:
     f_1 = io.BufferedReader(gz_1)
-    f_2 = io.BUfferedReader(gz_2)
+    f_2 = io.BufferedReader(gz_2)
 else:
     f_2 = gz_1
     f_2 = gz_2
@@ -49,8 +49,12 @@ select_ratio = select_ratio+1
 def pick_reads():
     dice = random.randint(0,RESOLUTION)
     if dice < select_ratio:
-        #pick
-        return True
+        dice2 = random.randint(0,ENTRIES_TO_BATCH)
+        if dice2 == 1:
+            #pick
+            return True
+        else:
+            return False
     else:
         return False
 
@@ -64,7 +68,7 @@ def list_of_lists_to_file(tb_1, fo_1,tb_2, fo_2):
         for j in i:
             fo_2.write(j)
 
-def check_exit(pc, ss,tc,ts, temp_batch):
+def check_exit(pc, ss,tc,ts, temp_batch_1, temp_batch_2):
     if tc >= ts:
         print "ERROR: File read completed without completing the sampling"
         #adds the entries in thetemp_batch buffer into the output file
@@ -91,11 +95,15 @@ temp_single_2 = []
 temp_batch_1 = []
 temp_batch_2 = []
 
-picked_count = 0
+picked_count = 1
 total_count = -1
 
 
+
 for line_1, line_2 in izip(f_1,f_2):
+    temp_single_1.append(line_1)
+    temp_single_2.append(line_2)
+
 
     if len(temp_single_1) == 4:
         #the entry has been read
@@ -105,8 +113,6 @@ for line_1, line_2 in izip(f_1,f_2):
         temp_single_2 = []
 
         check_exit(picked_count, sample_size, total_count,total_size,temp_batch_1,temp_batch_2)
-        temp_single_1.append(line_1)
-        temp_single_2.append(line_2)
         total_count = total_count + 1
 
         #status update
@@ -116,16 +122,12 @@ for line_1, line_2 in izip(f_1,f_2):
                 sys.stdout.write(message)
                 sys.stdout.flush()
                 sys.stdout.write('\b' * len(message))
-    else:
 
-        temp_single_1.append(line_1)
-        temp_single_2.append(line_2)
-
-    if len(temp_batch_1) == LINES_TO_BATCH:
+    if len(temp_batch_1) == ENTRIES_TO_BATCH:
         #the buffer is full, check to keep or discard
         if pick_reads():
             list_of_lists_to_file(temp_batch_1, f_out_1, temp_batch_2, f_out_2)
-            picked_count = picked_count + LINES_TO_BATCH
+            picked_count = picked_count + ENTRIES_TO_BATCH
         temp_batch_1 = []
         temp_batch_2 = []
 
