3807de3010ffd5bdff88d49e90ff63fc691f87e4
Wishva <wishvah@gmail.com>
2019-04-17 12:19:09 +1000

    r1 and r2 functionality

--- downsampler_simple.py
+++ downsampler_simple.py
@@ -1,4 +1,5 @@
 from __future__ import division
+from itertools import izip
 import gzip
 import sys
 import io
@@ -8,25 +9,38 @@ import random
 RESOLUTION = 100
 LINES_TO_BATCH = 100
 BUFFER = True
-gz_file_name = sys.argv[1].strip()
-total_size = int(sys.argv[2].strip())
-sample_size = int(sys.argv[3].strip())
-LINES_TO_BATCH = int(sys.argv[4].strip())
-gz = gzip.open(gz_file_name,'rb')
+
+gz_file_name_1 = sys.argv[1].strip()
+gz_file_name_2 = sys.argv[2].strip()
+total_size = int(sys.argv[3].strip())
+sample_size = int(sys.argv[4].strip())
+LINES_TO_BATCH = int(sys.argv[5].strip())
+gz_1 = gzip.open(gz_file_name_1,'rb')
+gz_2 = gzip.open(gz_file_name_2,'rb')
 if BUFFER:
-    f = io.BufferedReader(gz)
+    f_1 = io.BufferedReader(gz_1)
+    f_2 = io.BUfferedReader(gz_2)
 else:
-    f = gz
+    f_2 = gz_1
+    f_2 = gz_2
 
 
 #init the output file
-output_file = gz_file_name + "_downsampled_to_" + str(sample_size) + "_.gz"
-gz_out = gzip.open(output_file, "wb")
-if BUFFER:
-    f_out = io.BufferedWriter(gz_out)
-else:
-    f_out = gz_out
 
+
+output_file_1 = gz_file_name_1 + "_downsampled_to_" + str(sample_size) + "_.gz"
+output_file_2 = gz_file_name_2 + "_downsampled_to_" + str(sample_size) + "_.gz"
+gz_out_1 = gzip.open(output_file_1, "wb")
+gz_out_2 = gzip.open(output_file_2, "wb")
+
+if BUFFER:
+
+    f_out_1 = io.BufferedWriter(gz_out_1)
+    f_out_2 = io.BufferedWriter(gz_out_2)
+else:
+
+    f_out_1 = gz_out_1
+    f_out_2 = gz_out_2
 
 #randomizing stuff
 select_ratio = round(sample_size/total_size,int(RESOLUTION/10)) * RESOLUTION
@@ -41,58 +55,79 @@ def pick_reads():
         return False
 
 
-def list_of_lists_to_file(tb, fo):
-    for i in tb:
+def list_of_lists_to_file(tb_1, fo_1,tb_2, fo_2):
+    for i in tb_1:
         for j in i:
-            f_out.write(j)
+            fo_1.write(j)
+
+    for i in tb_2:
+        for j in i:
+            fo_2.write(j)
 
 def check_exit(pc, ss,tc,ts, temp_batch):
     if tc >= ts:
         print "ERROR: File read completed without completing the sampling"
         #adds the entries in thetemp_batch buffer into the output file
-        list_of_lists_to_file(temp_batch,f_out)
-        f.close()
-        f_out.close()
+
+        list_of_lists_to_file(temp_batch_1,f_out_1, temp_batch_2, f_out_2)
+        f_1.close()
+        f_2.close()
+        f_out_1.close()
+        f_out_2.close()
         sys.exit(1)
     if pc >= ss:
         #adds the entries in thetemp_batch buffer into the output file
-        list_of_lists_to_file(temp_batch,f_out)
-        list_of_lists_to_file(temp_batch,f_out)
-        f.close()
-        f_out.close()
+        list_of_lists_to_file(temp_batch_1,f_out_1, temp_batch_2, f_out_2)
+        f_1.close()
+        f_2.close()
+
+        f_out_1.close()
+        f_out_2.close()
         sys.exit(0)
 
-temp_single = []
-temp_batch = []
+temp_single_1 = []
+temp_single_2 = []
+
+temp_batch_1 = []
+temp_batch_2 = []
+
 picked_count = 0
 total_count = -1
-for line in f:
 
-    if len(temp_single) == 4:
 
-        check_exit(picked_count, sample_size, total_count,total_size,temp_batch)
+for line_1, line_2 in izip(f_1,f_2):
+
+    if len(temp_single_1) == 4:
         #the entry has been read
-        temp_batch.append(temp_single)
-        temp_single = []
-        temp_single.append(line)
+        temp_batch_1.append(temp_single_1)
+        temp_batch_2.append(temp_single_2)
+        temp_single_1 = []
+        temp_single_2 = []
+
+        check_exit(picked_count, sample_size, total_count,total_size,temp_batch_1,temp_batch_2)
+        temp_single_1.append(line_1)
+        temp_single_2.append(line_2)
         total_count = total_count + 1
 
         #status update
         if total_count % 10000 == 0:
             if picked_count != 0 and total_count != 0:
-                message = "Picked {} entries out of {}. pick: {}% , complete: {}% , read: {}%".format(picked_count, total_count, round((picked_count/total_count) * 100,2), round((picked_count/sample_size) * 100,2), round((total_count / total_size) * 100,2))
+                message = "Picked {} entries out of {}. pick_rate: {}% , pick_progress: {}% , current_state: {}%".format(picked_count, total_count, round((picked_count/total_count) * 100,2), round((picked_count/sample_size) * 100,2), round((total_count / total_size) * 100,2))
                 sys.stdout.write(message)
                 sys.stdout.flush()
                 sys.stdout.write('\b' * len(message))
     else:
-        temp_single.append(line)
 
-    if len(temp_batch) == LINES_TO_BATCH:
+        temp_single_1.append(line_1)
+        temp_single_2.append(line_2)
+
+    if len(temp_batch_1) == LINES_TO_BATCH:
         #the buffer is full, check to keep or discard
         if pick_reads():
-            list_of_lists_to_file(temp_batch, f_out)
+            list_of_lists_to_file(temp_batch_1, f_out_1, temp_batch_2, f_out_2)
             picked_count = picked_count + LINES_TO_BATCH
-        temp_batch = []
+        temp_batch_1 = []
+        temp_batch_2 = []
 
 
 
